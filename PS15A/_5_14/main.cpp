5a6215106ed4facc25d10e9284a03a603139ab03
U-PS14B22\vantan <akira206@gmail.com>
2015-05-14 22:36:43 +0900

    今日の答え

---
+++ PS15A/_5_14/main.cpp
@@ -0,0 +1,48 @@
+﻿
+#include "lib/framework.hpp"
+
+enum WindowSize {
+  WIDTH = 600,
+  HEIGHT = 400,
+};
+
+
+int main() {
+  AppEnv env(WIDTH, HEIGHT);
+
+  Random random;
+  random.setSeed(time(nullptr));
+
+  const int Point_Max = 10;
+  Vec2f pos[Point_Max];
+  float radius = 5;
+  bool  isActive[Point_Max];
+
+  for (int i = 0; i < Point_Max; ++i) {
+    pos[i] = Vec2f::Zero();
+    isActive[i] = false;
+  }
+
+  while (env.isOpen()) {
+    env.begin();
+
+    if (env.isPushButton(Mouse::LEFT)) {
+      for (int i = 0; i < Point_Max; ++i) {
+        if (!isActive[i]) {
+          isActive[i] = true;
+          pos[i] = Vec2f(random(-WIDTH / 2,  WIDTH / 2),
+                         random(-HEIGHT / 2, HEIGHT / 2));
+          break;
+        }
+      }
+    }
+
+    for (int i = 0; i < Point_Max; ++i) {
+      if (isActive[i]) {
+        drawPoint(pos[i].x(), pos[i].y(), radius, Color::white);
+      }
+    }
+
+    env.end();
+  }
+}
---
+++ PS15B/_5_14/main.cpp
@@ -0,0 +1,26 @@
+﻿
+#include "lib/framework.hpp"
+
+
+int main() {
+  AppEnv env(600, 400);
+
+  Vec2f pos   = Vec2f::Zero();
+  Vec2f size  = Vec2f(50, 50);
+  float speed = 4;
+
+  while (env.isOpen()) {
+    env.begin();
+
+    if (env.isPressKey('D')) { pos.x() += speed; } // 右に移動
+    if (env.isPressKey('A')) { pos.x() -= speed; } // 左に移動
+    if (env.isPressKey('W')) { pos.y() += speed; } // 上に移動
+    if (env.isPressKey('S')) { pos.y() -= speed; } // 下に移動
+
+    drawFillBox(pos.x(),  pos.y(),
+                size.x(), size.y(),
+                Color::white);
+
+    env.end();
+  }
+}
